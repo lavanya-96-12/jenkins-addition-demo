672b3dff299406bcec3060da6c98ee0fa8cb2c20
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-08-20 11:10:50 +0530

    Update All Arithmetic Functions

--- addition.py
+++ addition.py
@@ -2,6 +2,12 @@ a=30
 b=20
 
 result=a+b
+result1=a-b
+result2=a*b
+result3=a/b
 print("First Number:", a);
 print("Second Number:", b)
 print("Sum:", result)
+print("Difference:", result1)
+print("Product:", result2)
+print("Quotient:", result3)
